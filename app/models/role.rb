07a5dc4a4f656a9e7e338a1bf37f05479adadec1
gerardo colina <gerardocolina@gmail.com>
2017-11-12 16:34:27 -0400

    added asociation between user and role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,5 @@
 class Role < ApplicationRecord
 	has_and_belongs_to_many :options
 	validates_presence_of :nombre, :descripcion
+	has_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109041240) do
+ActiveRecord::Schema.define(version: 20171112202152) do
 
   create_table "options", force: :cascade do |t|
     t.string "nombre"
@@ -46,8 +46,10 @@ ActiveRecord::Schema.define(version: 20171109041240) do
     t.string "identification"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
 end
